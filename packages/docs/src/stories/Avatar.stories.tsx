e79743e2cfe4f457a2f31939b98c540fd3ab27bc
eugustavo <gustavo.ifto@gmail.com>
2022-10-22 14:22:49 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@eugustavo/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/eugustavo.png',
+    alt: 'Gustavo Souza',
+  },
+  argTypes: {
+    src: {
+      control: { type: 'text' },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: '',
+  },
+}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@eugustavo/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum veritatis ullam molestias culpa consectetur beatae. Illum iure, voluptates ad pariatur magni eligendi adipisci laborum, animi omnis voluptas optio itaque.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: { type: 'inline-radio' },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
